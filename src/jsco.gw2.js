36f27b75b84654e0881c1167ce62c8013b12cda2
bluelovers <codelovers@users.sourceforge.net>
2014-04-25 11:51:29 +0800

    demo (Step: 1)
    Step: 1

--- src/jsco.gw2.js
+++ src/jsco.gw2.js
@@ -1,30 +1,33 @@
-(function($){
+(function($)
+{
 	var unsafeWindow = unsafeWindow || window;
 
 	var _gw2api = new GW2API;
-	var _gw2 = (function (){
+	var _gw2 = new(function()
+	{
 		this._self = this;
 	})();
 
-	$.extend({
-		gw2 : _gw2,
+	$.extend(
+	{
+		gw2: _gw2,
 	});
 
 	$.extend($.gw2, {
-		api : _gw2api,
+		api: _gw2api,
 
 		/**
 		 * http://wiki.guildwars2.com/index.php?title=MediaWiki:GameLinks.js
 		 **/
-		gamelink : function(type, id){
+		gamelink: function(type, id)
+		{
 			var typeId = '0';
 
 			if (type.match(/(1|2|4|7|8|10)/))
 			{
 				typeId = String.fromCharCode(type);
 
-				if (type == 2)
-					typeId += String.fromCharCode(1);
+				if (type == 2) typeId += String.fromCharCode(1);
 			}
 			else if ($.gw2.gamelink.typeid[type])
 			{
@@ -47,47 +50,51 @@
 	});
 
 	$.extend($.gw2.gamelink, {
-		typeid : {
-			item : String.fromCharCode(2) + String.fromCharCode(1),
-			map : String.fromCharCode(4),
-			skill : String.fromCharCode(7),
-			trait : String.fromCharCode(8),
-			recipe : String.fromCharCode(10),
+		typeid: {
+			item: String.fromCharCode(2) + String.fromCharCode(1),
+			map: String.fromCharCode(4),
+			skill: String.fromCharCode(7),
+			trait: String.fromCharCode(8),
+			recipe: String.fromCharCode(10),
 		},
 	});
 
-	var Base64 = {
+	var Base64 =
+	{
 		// private property
-		_keyStr : "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/=",
+		_keyStr: "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/=",
 
 		// public method for encoding
-		encode : function (input) {
-		var output = "";
-		var chr1, chr2, chr3, enc1, enc2, enc3, enc4;
-		var i = 0;
+		encode: function(input)
+		{
+			var output = "";
+			var chr1, chr2, chr3, enc1, enc2, enc3, enc4;
+			var i = 0;
 
-		while (i < input.length) {
-			chr1 = input.charCodeAt(i++);
-			chr2 = input.charCodeAt(i++);
-			chr3 = input.charCodeAt(i++);
+			while (i < input.length)
+			{
+				chr1 = input.charCodeAt(i++);
+				chr2 = input.charCodeAt(i++);
+				chr3 = input.charCodeAt(i++);
 
-			enc1 = chr1 >> 2;
-			enc2 = ((chr1 & 3) << 4) | (chr2 >> 4);
-			enc3 = ((chr2 & 15) << 2) | (chr3 >> 6);
-			enc4 = chr3 & 63;
+				enc1 = chr1 >> 2;
+				enc2 = ((chr1 & 3) << 4) | (chr2 >> 4);
+				enc3 = ((chr2 & 15) << 2) | (chr3 >> 6);
+				enc4 = chr3 & 63;
 
-			if (isNaN(chr2)) {
-				enc3 = enc4 = 64;
-			} else if (isNaN(chr3)) {
-				enc4 = 64;
+				if (isNaN(chr2))
+				{
+					enc3 = enc4 = 64;
+				}
+				else if (isNaN(chr3))
+				{
+					enc4 = 64;
+				}
+
+				output = output + this._keyStr.charAt(enc1) + this._keyStr.charAt(enc2) + this._keyStr.charAt(enc3) + this._keyStr.charAt(enc4);
 			}
 
-			output = output +
-			this._keyStr.charAt(enc1) + this._keyStr.charAt(enc2) +
-			this._keyStr.charAt(enc3) + this._keyStr.charAt(enc4);
-		}
-
-		return output;
+			return output;
 		}
 	};
 
