569a0b500e4012e3c5ae4d7545934fa11348d29b
bluelovers <codelovers@users.sourceforge.net>
2014-04-26 11:12:52 +0800

    require.js (Step: 4)
    Step: 4

---
+++ src/main.js
@@ -0,0 +1,49 @@
+requirejs.config(
+{
+	'baseUrl': '..',
+
+	paths: {
+		'css': 'lib/requirejs/plugin/css',
+		'order': 'lib/requirejs/plugin/order',
+
+		'jquery': 'https://ajax.googleapis.com/ajax/libs/jquery/1/jquery.min',
+
+		'leaflet' : 'http://cdn.leafletjs.com/leaflet-0.7.2/leaflet',
+		'leaflet-css' : 'http://cdn.leafletjs.com/leaflet-0.7.2/leaflet',
+
+		'gw2' : 'src/jsco.gw2.js',
+		'gw2api' : 'src/gw2-api-wrapper.js',
+	},
+
+	shim: {
+		'jquery': {
+			exports: '$',
+		},
+
+		'leaflet':
+		{
+			deps: ['css!leaflet-css'],
+			exports: 'L',
+		},
+
+		'gw2': {
+			deps: ['order!jquery', 'order!gw2api'],
+		},
+
+		'gw2api': {
+			deps: ['jquery'],
+		},
+	},
+});
+
+require(['order!jquery'], function()
+{
+	 console.log($);
+});
+
+require(["leaflet"], function()
+{
+    console.log(L);
+});
+
+//define(['css!leaflet-css', 'leaflet'], { });
