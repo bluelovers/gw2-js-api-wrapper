328b334b90ce1484a97115fed119efb66e3b6063
bluelovers <codelovers@users.sourceforge.net>
2014-05-10 11:41:47 +0800

    update (Step: 6)
    Step: 6

--- src/main.js
+++ src/main.js
@@ -7,23 +7,45 @@ requirejs.config(
 		'order': 'lib/requirejs/plugin/order',
 		'text': 'lib/requirejs/plugin/text',
 
+		'load': 'lib/requirejs/plugin/load',
+
 		'jquery': 'https://ajax.googleapis.com/ajax/libs/jquery/1/jquery.min',
+		'jquery.selector.data': 'lib/jquery/plugin/jquery.selector.data',
 
-		'leaflet' : 'http://cdn.leafletjs.com/leaflet-0.7.2/leaflet',
-		'leaflet-css' : 'http://cdn.leafletjs.com/leaflet-0.7.2/leaflet',
+		'leaflet': 'http://cdn.leafletjs.com/leaflet-0.7.2/leaflet',
+		'leaflet-css': 'http://cdn.leafletjs.com/leaflet-0.7.2/leaflet',
 
-		'gw2' : 'src/jsco.gw2.js',
-		'gw2api' : 'src/gw2-api-wrapper.js',
+		'gw2': 'src/jsco.gw2.js',
+		'gw2api': 'src/gw2-api-wrapper.js',
+
+		'gw2map': 'src/gw2/gw2map.js',
+
+		'propertyParser': 'lib/requirejs/plugin/propertyParser',
+	},
+
+	map: {
+		'*': {
+
+		},
 	},
 
 	shim: {
 		'jquery': {
+			deps: [],
 			exports: '$',
+
+			init: function()
+			{
+				console.log(['init', $]);
+			},
 		},
 
-		'leaflet':
-		{
-			deps: ['css!leaflet-css', 'jquery'],
+		'jquery.selector.data': {
+			deps: ['jquery'],
+		},
+
+		'leaflet': {
+			deps: ['load!leaflet-css', 'jquery'],
 			exports: 'L',
 		},
 
@@ -34,12 +56,16 @@ requirejs.config(
 		'gw2api': {
 			deps: ['jquery'],
 		},
+
+		'gw2map': {
+			deps: ['order!jquery''order!gw2api', 'order!gw2'],
+		},
 	},
 });
 
 require(['order!jquery'], function()
 {
-	 console.log($);
+	console.log($);
 });
 
 //require(["leaflet"], function()
