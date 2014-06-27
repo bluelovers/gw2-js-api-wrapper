84ebe05a4e2ff08c3c822a518380416918fbd846
bluelovers <codelovers@users.sourceforge.net>
2014-06-27 19:24:26 +0800

    . (Step: 6)
    Step: 6

--- examples/app/test.js
+++ examples/app/test.js
@@ -1,18 +1,40 @@
 requirejs.config(
 {
 	'baseUrl': '..',
+});
 
-	paths: {
-		'requirejs': 'lib/requirejs',
-	},
-
-	deps: ['requirejs/bootstrap'],
-
-	callback: function(r)
+require(['examples/bootstrap'], function(r)
+{
+	requirejs.config(
 	{
+		paths: {
+			'gw2api': 'src/gw2/gw2api',
+		},
 
-		var L = require.toUrl('css');
+		//deps: ['lib/requirejs/bootstrap'],
 
-		console.log([r, L, require.toUrl('order'), require.toUrl('requirejs')]);
-	},
+		callback: function(r)
+		{
+			var a = require([]);
+
+			var L = require.toUrl('css');
+
+			console.log([r, L, require.toUrl('gw2api'), require.toUrl('order'), require.toUrl('requirejs'), require.toUrl('jquery'), require.s.contexts._.config.paths]);
+
+			require(['gw2api'], function(gw2api)
+			{
+				console.log([gw2api]);
+
+				var i;
+
+				for (i in gw2api.apiMap)
+				{
+					if (i != '_')
+					{
+						console.log(gw2api.fn.camelCase(i));
+					}
+				}
+			});
+		},
+	});
 });
