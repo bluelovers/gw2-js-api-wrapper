5bb83fb3d88f7275cd054d2d82ce2144992373a0
bluelovers <codelovers@users.sourceforge.net>
2014-07-01 11:11:25 +0800

    . (Step: 1)
    Step: 1

--- examples/app/test.js
+++ examples/app/test.js
@@ -14,16 +14,16 @@ requirejs.config(
 
 		var L = require.toUrl('css');
 
-		console.log([r, L, require.toUrl('gw2api'), require.toUrl('order'), require.toUrl('requirejs'), require.toUrl('jquery'), require.s.contexts._.config.paths]);
+		console.log([a, r, L, require.toUrl('gw2api'), require.toUrl('order'), require.toUrl('requirejs'), require.toUrl('jquery'), require.s.contexts._.config.paths]);
 
-		require(['gw2api'], function(gw2api)
+		require(['gw2api', 'scriptpath'], function(gw2api, scriptpath)
 		{
 			console.log([gw2api, gw2api.apiMap._.alias]);
 
 			console.log(gw2api.get('maps', 15));
 			console.log(gw2api.get('maps', 16));
 
-			console.log([gw2api.Cache]);
+			console.log([gw2api.Cache, scriptpath()]);
 		});
 	},
 });
