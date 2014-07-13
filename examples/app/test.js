0ba6871b99e4b37b84e44560f026c4cb443bebe3
bluelovers <codelovers@users.sourceforge.net>
2014-07-13 15:15:40 +0800

    . (Step: 1)
    Step: 1

--- examples/app/test.js
+++ examples/app/test.js
@@ -28,9 +28,54 @@ requirejs.config(
 			var a;
 
 
-			console.log([a = gw2api.get('chat/link/encode', 'item', 49532, 250), gw2api.ChatLink.decode(a), gw2api.ChatLink.decode('&AvtgTAAA')]);
+			console.log([a = gw2api.get('chat/link/encode', 'item', 49532, 260), gw2api.ChatLink.decode(a), gw2api.ChatLink.decode('&AvtgTAAA')]);
 
 			console.log([gw2api.ChatLink(), gw2api()]);
+
+
+			console.log([require(['gw2api']), require('gw2api')]);
 		});
+
+		require(['jquery']);
+
+
+		var stripUri = function(uri, base)
+		{
+			if (!uri) return uri;
+
+			while (uri.match(/\/[^\/]+\/..(?:\/|$)/))
+			{
+				uri = uri.replace(/\/[^\/]+\/..(\/|$)/ig, '$1');
+			}
+
+			while (uri.match(/\/{2,}|\/.\//ig))
+			{
+				uri = uri.replace(/\/{2,}|\/.\//ig, '/');
+			}
+
+			uri = uri.replace(/\/+$/ig, '') + '/';
+
+			if (base && (base = stripUri(base)))
+			{
+				if (uri.substring(0, base.length) === base)
+				{
+					uri = uri.substring(base.length);
+				}
+			}
+
+			return uri;
+		};
+
+		require(['scriptpath'], function(scriptpath)
+		{
+			var a = getScriptPath(
+			{
+				basePath: require.s.head.baseURI.replace(/(\/)[^\/]+$/, '$1') + require.s.contexts._.config.baseUrl,
+			});
+
+			console.log([scriptpath, a, a.fullPath()]);
+		});
+
+		console.log([require, require.s, stripUri(require.s.head.baseURI.replace(/(\/)[^\/]+$/, '$1') + require.s.contexts._.config.baseUrl, 'file:/D:/Users/Documents/The%20Project/gw2-js-api-wrapper/')]);
 	},
 });
